ed0fa9b90849beb2390b9878134c03bb3ce2998e
MadmanMigz <202310011@fit.edu.ph>
2025-05-16 16:30:46 +0800

    refactor (page): updated code to use the arrays.

--- page/index.php
+++ page/index.php
@@ -12,30 +12,33 @@ $stances = [
     "Southpaw" => ["Left-handed stance (right foot forward)", "southpaw.jpg"],
     "Square" => ["Feet parallel to each other, facing the opponent completely (Example of this stance are Mike Tyson, Floyd Patterson, Connor Benn).", "square.jpg"]
 ];
-$fightingStyle = "Slugger/Brawler Style";
-$styleDescription = "Focuses and relies on his own power and constant forward movement without all that fancy footwork. <br>
-Users of this style are usually heavyweights and light heavyweights or just someone who was born with natural power. <br>";
+
+$boxingStyles = getBoxingStyles();
+$fightingStyle = array_key_first($boxingStyles);
+$styleDescription = $boxingStyles[$fightingStyle];
 $styleImage = "slugger-style.jpg";
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $pageTitle; ?> - Madman's Boxing</title>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
 <body>
     <?php include '../components/index.php'; ?>
-    
+
     <div class="main-container">
         <h1><?php echo $pageTitle; ?></h1>
-        
+
         <div class="section">
             <h2>Basic Punches</h2>
             <div class="card-container">
-                <?php foreach($basicPunches as $name => $data): ?>
+                <?php foreach ($basicPunches as $name => $data): ?>
                     <div class="card">
                         <img src="../assets/img/<?php echo $data[1]; ?>" alt="<?php echo $name; ?>">
                         <div class="card-content">
@@ -46,7 +49,7 @@ $styleImage = "slugger-style.jpg";
                 <?php endforeach; ?>
             </div>
         </div>
-        
+
         <div class="section">
             <h2>Fighting Style</h2>
             <div class="featured-card">
@@ -57,11 +60,11 @@ $styleImage = "slugger-style.jpg";
                 </div>
             </div>
         </div>
-        
+
         <div class="section">
             <h2>Stances</h2>
             <div class="card-container">
-                <?php foreach($stances as $name => $data): ?>
+                <?php foreach ($stances as $name => $data): ?>
                     <div class="card">
                         <img src="../assets/img/<?php echo $data[1]; ?>" alt="<?php echo $name; ?>">
                         <div class="card-content">
@@ -72,10 +75,11 @@ $styleImage = "slugger-style.jpg";
                 <?php endforeach; ?>
             </div>
         </div>
-        
+
         <div class="back-button">
             <a href="../index.php">Back to Home</a>
         </div>
     </div>
 </body>
+
 </html>
