35fabb6b403da8d2658d108cfcb24e674ab9e25b
MadmanMigz <202310011@fit.edu.ph>
2025-05-16 16:24:59 +0800

    feat (utils): added getboxingstyles as utils.

---
+++ utils/config.util.php
@@ -0,0 +1,9 @@
+<?php
+function getBoxingStyles()
+{
+    return [
+        "Slugger/Brawler" => "Focuses on power and forward movement",
+        "Out-boxer" => "Focuses on footwork and maintaining distance",
+        "Swarmer" => "Focuses on constant pressure and volume punching"
+    ];
+}
